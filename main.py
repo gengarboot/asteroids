a19f15419bbe13e1834d38257cd1027542c76a70
gengarboot <gengarboot@fastmail.com>
2025-06-21 18:10:35 +0200

    cleaning out old print statements

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ def main():
 				return
 		screen.fill("black")
 		pygame.display.flip()
-	print("Starting Asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
-
 
 if __name__ == "__main__":
 	main()
