e3b08330128008ef2b23db7c75ad37f8f5fedb36
gengarboot <gengarboot@fastmail.com>
2025-06-22 09:27:12 +0200

    adding clock to handle deltatime

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
@@ -10,6 +12,9 @@ def main():
 				return
 		screen.fill("black")
 		pygame.display.flip()
+
+		# Set frame rate to 60 - tick returns how longs since last time it was called
+		dt = clock.tick(60) 
 
 if __name__ == "__main__":
 	main()
