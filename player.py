e1fff42e75e03e9c0eddf82b42488f294e90b5bb
gengarboot <gengarboot@fastmail.com>
2025-06-22 10:00:24 +0200

    make the player spin like a crazy little triangle

--- player.py
+++ player.py
@@ -19,3 +19,17 @@ class Player(CircleShape):
 		b = self.position - forward * self.radius - right
 		c = self.position - forward * self.radius + right
 		return [a, b, c]
+
+	def rotate(self, dt):
+		self.rotation += PLAYER_TURN_SPEED * dt
+
+	def update(self, dt):
+		keys = pygame.key.get_pressed()
+
+		# left rotation
+		if keys[pygame.K_a]:
+			self.rotate(-dt)
+
+		# right rotation
+		if keys[pygame.K_d]:
+			self.rotate(dt)
